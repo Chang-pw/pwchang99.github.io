ecff29462e7ca22fa13530a7abbe0753f8d69786
Chang-pw <changpw0909@gmail.com>
2026-06-02 20:46:10 +0800

    modified:   .gitignore

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,7 @@
-bundle exec jekyll liveserve
+#!/usr/bin/env bash
+set -e
+
+export GEM_HOME="$PWD/vendor/bundle/ruby/2.6.0"
+export GEM_PATH="$GEM_HOME:${HOME}/.gem/ruby/2.6.0"
+
+bundle exec jekyll serve --livereload --host 127.0.0.1
